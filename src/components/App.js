6a11a25957215b6e3b72c6bae65bba36ab9cb0a1
kellyeryan <kellyeryan@gmail.com>
2020-03-02 16:16:54 -0500

    Finish props and state react lab

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,31 @@ class App extends React.Component {
     }
   }
 
+  onFindPetsClick = () => {
+    let petLink = "/api/pets"
+    if (this.state.filters.type !== "all") {
+      petLink += `?type=${this.state.filters.type}`
+    }
+    fetch(petLink)
+      .then(response => response.json())
+      .then(pets => this.setState({ pets }))
+  }
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets });
+  };
+
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        ...this.state.filters, type: event.target.value
+      }
+    });
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +49,14 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                  onChangeType={this.onChangeType}
+                  onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+              pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
